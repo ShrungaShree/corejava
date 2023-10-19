a04e40ca76b4416702fa43c76786e69c7f766e95
Shrunga Shree <shrungashree29@gmail.com>
2023-10-19 16:31:54 +0530

    modified

--- Corejavaprograms/src/com/one/HelloWorld.java
+++ Corejavaprograms/src/com/one/HelloWorld.java
@@ -4,12 +4,13 @@ public class HelloWorld {
 public static void main(String[] args) {
 	 HashSet<String> stringSet = new HashSet<>();
 	 stringSet.add("Apple");
-     stringSet.add("Banana");
+    
      stringSet.add("watermelon");
      stringSet.add("Gauva");
      System.out.println("HashSet elements: " + stringSet);
      stringSet.remove("Banana");
      System.out.println("HashSet elements after removing: " + stringSet);
+     System.out.println("new line");
 
 
 }
