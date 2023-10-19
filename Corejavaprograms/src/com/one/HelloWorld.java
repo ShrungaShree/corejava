2b4fc4312a4e04e1c6e8e76694d6fe5c4183d3c2
Shrunga Shree <shrungashree29@gmail.com>
2023-10-19 10:58:10 +0530

    My first commit

---
+++ Corejavaprograms/src/com/one/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.one;
+import java.util.HashSet;
+public class HelloWorld {
+public static void main(String[] args) {
+	 HashSet<String> stringSet = new HashSet<>();
+	 stringSet.add("Apple");
+     stringSet.add("Banana");
+     stringSet.add("watermelon");
+     stringSet.add("Gauva");
+     System.out.println("HashSet elements: " + stringSet);
+     stringSet.remove("Banana");
+     System.out.println("HashSet elements after removing: " + stringSet);
+
+
+}
+}
